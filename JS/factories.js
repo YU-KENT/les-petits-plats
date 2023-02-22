8b694795aadc676cbb7ad0ccfdb4e21290f82db5
YU KENT <tianyu.tina@gmail.com>
2023-02-22 14:54:43 +0100

    close Tag

--- JS/factories.js
+++ JS/factories.js
@@ -1,6 +1,6 @@
 function recipesFactory(recipes,ingredients) {
 
- const{ name, time, description,id} = recipes
+ const{ name, time, description,id,appliance, ustensils} = recipes
  const{ingredient, quantity, unit} = ingredients
 
  function getRecipesCard(){
@@ -22,6 +22,8 @@ function recipesFactory(recipes,ingredients) {
             <div class="col-6 d-flex flex-column rec_ingredients">
             </div>
             <p class="col-6 description">${description}</p>
+            <p>${appliance}</p>
+            <p>${ustensils}</p>
          </div>
       </div>
    </div>
@@ -51,7 +53,7 @@ function getIngredientCard(){
   })
   }
 
-return {name, time, description,ingredient, quantity, unit, id, getRecipesCard,  getIngredientCard}
+return {name, time, description,ingredient, quantity, unit, id,appliance,ustensils, getRecipesCard,  getIngredientCard}
 
 }
 
@@ -59,7 +61,7 @@ const divIngredientsList = document.querySelector(".ingredients-list")
 const divAppareilsList = document.querySelector(".appareils-list")
 const divUstensilesList = document.querySelector(".ustensiles-list")
 
-function displayingredientsList(arry){
+function displayingredientList(arry){
 
   for(let i = 0; i < arry.length; i++){
     const newP = document.createElement("p")
@@ -70,15 +72,46 @@ function displayingredientsList(arry){
 
 }
 
-function displayTag(list){
+function displayAppareilsList(arry){
+
+  for(let i = 0; i < arry.length; i++){
+    const newP = document.createElement("p")
+    newP.classList.add("col","list")
+    newP.textContent = arry[i]
+    divAppareilsList.appendChild(newP)
+  }
+
+}
+function displayUstensilesList(arry){
+
+  for(let i = 0; i < arry.length; i++){
+    const newP = document.createElement("p")
+    newP.classList.add("col","list")
+    newP.textContent = arry[i]
+    divUstensilesList.appendChild(newP)
+  }
+
+}
+
+
+function tagFactory(list,e){
 const divTag = document.querySelector(".div-tag")
 const newDiv = document.createElement("div")
 newDiv.classList.add("tag")
 newDiv.innerHTML = `
 <span>${list}</span>
-<span class="button-close"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="white" d="M12 20c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m0-18C6.47 2 2 6.47 2 12s4.47 10 10 10s10-4.47 10-10S17.53 2 12 2m2.59 6L12 10.59L9.41 8L8 9.41L10.59 12L8 14.59L9.41 16L12 13.41L14.59 16L16 14.59L13.41 12L16 9.41L14.59 8Z"/></svg>
+<span class="button-close" onclick="closeTag(this)"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="white" d="M12 20c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m0-18C6.47 2 2 6.47 2 12s4.47 10 10 10s10-4.47 10-10S17.53 2 12 2m2.59 6L12 10.59L9.41 8L8 9.41L10.59 12L8 14.59L9.41 16L12 13.41L14.59 16L16 14.59L13.41 12L16 9.41L14.59 8Z"/></svg>
 </span>
 `
 divTag.style.display ="flex"
 divTag.appendChild(newDiv)
+const targetChamp = e.target.parentNode.classList.toString()
+
+if(targetChamp.includes("ingredients")){
+  newDiv.classList.add("ingredients")
+}else if (targetChamp.includes("appareils")){
+  newDiv.classList.add("appareils")
+}else if (targetChamp.includes("ustensiles")){
+  newDiv.classList.add("ustensiles")
+}
 }
--- JS/main.js
+++ JS/main.js
@@ -38,17 +38,15 @@ async function init(){
     displayRecipes(allRecipes);
     
     const ustensilsList = noRepeatArray(ustensilsData) 
-    console.log("ustensilsList",ustensilsList)
-
+  
     const appareilsList = noRepeatArray(appareilsData)
-    console.log("appareilsList",appareilsList)
 
     const ingreArry = getIngredientsList(ingredientsData)
     const ingredientsList = noRepeatArray(ingreArry)
-    console.log("ingredientsList",ingredientsList)
     
-
-    displayingredientsList(ingredientsList)
+    displayingredientList(ingredientsList);
+    displayAppareilsList(appareilsList);
+    displayUstensilesList(ustensilsList);
 
     /////////////////////////
     /* filterSearchBar1(allRecipes) */ 
@@ -56,117 +54,193 @@ async function init(){
 }
 init();
 
+//search bar
 const searchBar = document.getElementById("search-bar")
 searchBar.addEventListener('keyup', e =>{
-    const recipesCard = document.querySelectorAll(".recipes_card")
+    
     const searchedLetters = e.target.value.toLowerCase().replace(/\s/g, "")
     console.log(searchedLetters);
-    filterSearchBar(searchedLetters,recipesCard);
+    filterSearchBar(searchedLetters);
 })
 
-function filterSearchBar(letters,card){
+function filterSearchBar(letters){
+    const recipesCard = document.querySelectorAll(".recipes_card:not(.nonvisible)")
+    console.log("recipesCard",recipesCard,letters)
     
     if (letters.length > 2){
-        for( let i = 0; i < card.length; i ++){
-            if(card[i].textContent.toLowerCase().includes(letters)){
-                card[i].style.display ="flex";
-                console.log(card[i].textContent)
+        for( let i = 0; i < recipesCard.length; i ++){
+            if(recipesCard[i].textContent.toLowerCase().replace(/\s/g, "").includes(letters)){
+                recipesCard[i].classList.remove("nonvisible")
+                
             }else{
-                card[i].style.display ="none";
+                recipesCard[i].classList.add("nonvisible")
          }
         }
+        }
+    checkResult();
+    
+}
+function checkResult(){
+    
+    const restCard = document.querySelectorAll(".recipes_card:not(.nonvisible)")
+    const divSearchbar = document.querySelector(".search-bar")
+  /*   console.log("checkResult",restCard,divSearchbar) */
+    if(restCard.length == 0){
+        divSearchbar.setAttribute("data-error", "Vous pouvez chercher «tarte aux pommes », « poisson », etc...");
+        divSearchbar.setAttribute("data-error-visible", true)
+    }
+    else{
+        divSearchbar.removeAttribute("data-error");
+        divSearchbar.removeAttribute("data-error-visible")
     }
 }
 
-// rechercher dans le champ d'ingredient
-const champIngredients = document.getElementById("search-champs_ingredients")
-const divIngreList = document.querySelector(".ingredients-list")
+// search par 3 champs
 
+const champIngredients = document.getElementById("search-champs_ingredients")
+const champAppareils =  document.getElementById("search-champs_appareils")
+const champUstensiles =  document.getElementById("search-champs_ustensiles")
+const divIngreList = document.querySelector(".ingredients-list")
 
 const divApparList = document.querySelector(".appareils-list")
 const appraListLength = divApparList.length
 const divUstenList = document.querySelector(".ustensiles-list")
 const ustenListLength = divUstenList.length
-
-
-champIngredients.addEventListener('keyup', (e) =>{
-    
-    const IngredientsList = document.querySelectorAll(".ingredients-list p")
-    const searchedLetters = e.target.value.toLowerCase().replace(/\s/g, "")
-    
-    
-    filterChamps(searchedLetters,IngredientsList)
-    divIngreList.style.display = "flex"; // montrer le resultat de recherche
-    divIngreList.style.flexDirection = "column";
-    creatTag(IngredientsList);
-    
-})
 // function filtre par mot
 function filterChamps(letters,list){
-    if (letters.length > 2){
+    console.log("filterChamps")
+    
         for( let i = 0; i < list.length; i ++){
-            if(list[i].textContent.toLowerCase().includes(letters)){
+            if(list[i].textContent.toLowerCase().replace(/\s/g, "").includes(letters)){
                 list[i].classList.remove("nonvisible")
                 
             }else{
                 list[i].classList.add("nonvisible")
                  }
-        }}
+        }
+}
+// search ingredients
+
+champIngredients.addEventListener('keyup', (e) =>{
     
+    const IngredientsList = document.querySelectorAll(".ingredients-list p")
+    const searchedLetters = e.target.value.toLowerCase().replace(/\s/g, "")
+    if(e.target.value.length > 2){
+    filterChamps(searchedLetters,IngredientsList)
+    divIngreList.style.display = "flex"; // montrer le resultat de recherche
+    divIngreList.style.flexDirection = "column";
 
-}
-
-function cleanClassNonvisible(){
-    /* const a = _this.parentNode.lastElementChild.childNodes */
-    const visibleList = document.querySelectorAll(".list.nonvisible")
-    visibleList.forEach((list)=>list.classList.remove("nonvisible"))
-}
-
-function changeArrow(_this){
-   const arrowDown = _this.nextElementSibling;
-   const arrowUp = arrowDown.nextElementSibling;
-   arrowDown.style.visibility= "hidden";
-   arrowUp.style.visibility= "visible";
-}
-
-function gestionListApresBlur(_this){
-    const arrowDown = _this.nextElementSibling;
-    const arrowUp = arrowDown.nextElementSibling;
-    if(_this.value ==""){
-        const parentList = _this.parentNode.lastElementChild
-
-        parentList.style.display = "none";
-        _this.style.width='170px',
-        cleanClassNonvisible();
-        arrowDown.style.visibility= "visible";
-        arrowUp.style.visibility= "hidden";
+    creatTag(IngredientsList)
+   
     }
+})
 
+//seaarch Appareils
+champAppareils.addEventListener('keyup', (e) =>{
+    
+    const AppareilsList = document.querySelectorAll(".appareils-list p")
+    const searchedLetters = e.target.value.toLowerCase().replace(/\s/g, "")
+    if(e.target.value.length > 2){
+    filterChamps(searchedLetters,AppareilsList)
+    divApparList.style.display = "flex"; // montrer le resultat de recherche
+    divApparList.style.flexDirection = "column";
+    creatTag(AppareilsList)}
+    
+})
+//search ustensiles
+champUstensiles.addEventListener('keyup', (e) =>{
+    
+    const ustensilesList = document.querySelectorAll(".ustensiles-list p")
+    const searchedLetters = e.target.value.toLowerCase().replace(/\s/g, "")
+    if(e.target.value.length > 2){
+    filterChamps(searchedLetters,ustensilesList)
+    divUstenList.style.display = "flex"; // montrer le resultat de recherche
+    divUstenList.style.flexDirection = "column";
+    
+    creatTag(ustensilesList)}
+    
+})
+
+
+function cleanListNonvisible(){
+    const nonvisibleList = document.querySelectorAll(".list.nonvisible")
+    nonvisibleList.forEach((list)=>list.classList.remove("nonvisible"))
 }
+function cleanCardNonvisible(){
+    const nonvisibleCard = document.querySelectorAll(".recipes_card.nonvisible")
+    nonvisibleCard.forEach((list)=>list.classList.remove("nonvisible"))
+}
+
+//creat Tag
 var tagList = [];
 function creatTag(lists){
-    
+   
+    console.log("creatTag")
     const parentList = lists[0].parentNode
+
     for(let i = 0; i < lists.length; i++){
     lists[i].addEventListener("click",(e)=>{
         
         const listSelected = e.target.textContent
+        const motDeCle = listSelected.toLowerCase().replace(/\s/g, "")
         if(tagList.indexOf(listSelected) == -1){
-           displayTag(listSelected);
+           tagFactory(listSelected,e);
            tagList.push(listSelected);
-           console.log('taglist',tagList,e.target )
+           console.log('taglist',tagList,e.target.parentNode.classList)
            parentList.style.display = "none";
-           cleanClassNonvisible();
-           
-        }else{
-            console.log("KKK",listSelected,e.target)
-            console.log(tagList.indexOf(listSelected))
-    }
+           cleanListNonvisible();
+           filterSearchBar(motDeCle) 
+        }
+        else return
+            
     })
     }
 }
+// close tag
+function closeTag(_this){
+   console.log(tagList[0])
+   const btnsClose = document.querySelectorAll(".div-tag .button-close")
+   const tagLetters = _this.previousElementSibling.textContent
+   console.log("KKK",btnsClose,tagLetters)
+   _this.parentNode.remove();
+   removeByValue(tagList, tagLetters)
+   cleanCardNonvisible();
+   for(let i=0 ; i< tagList.length; i++){
+    const restTagLetters = tagList[i].toLowerCase().replace(/\s/g, "")
+    filterSearchBar(restTagLetters)
+   }
+  
+}
+// remove un value dans taglist
+function removeByValue(arr, val) {
+    for(var i = 0; i < arr.length; i++) {
+     if(arr[i] == val) {
+      arr.splice(i, 1);
+      break;
+     }}}
 
-
+function changeArrow(_this){
+    const arrowDown = _this.nextElementSibling;
+    const arrowUp = arrowDown.nextElementSibling;
+    arrowDown.style.visibility= "hidden";
+    arrowUp.style.visibility= "visible";
+ }
+ 
+ function gestionListApresBlur(_this){
+     const arrowDown = _this.nextElementSibling;
+     const arrowUp = arrowDown.nextElementSibling;
+     if(_this.value ==""){
+         const parentList = _this.parentNode.lastElementChild
+ 
+         parentList.style.display = "none";
+         _this.style.width='170px',
+         cleanListNonvisible();
+         arrowDown.style.visibility= "visible";
+         arrowUp.style.visibility= "hidden";
+     }
+ 
+ }
+ 
 
 /////////////////////
 /*  function filterSearchBar1(allRecipes){
